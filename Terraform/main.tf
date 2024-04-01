3291d8578573389f76c00a8f666ad3f64946125d
efern62 <efern@outlook.com>
2024-04-01 06:38:53 -0700

    Remove main.tf

--- Terraform/main.tf
+++
@@ -1,9 +0,0 @@
-data "terraform_remote_state" "iac_state" {
-  backend = "gcs"
- # config = {
-   # bucket = "hmh-sand-terraform-state-sandboxes"
-   # prefix = "Teams/Server_Engineering"
- # }
-locals {
-  sandbox_outputs = data.terraform_remote_state.iac_state.outputs.sandbox_projects.sandbox_outputs
-}
